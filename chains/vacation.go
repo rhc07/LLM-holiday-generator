14143f227ad81ed6c1b3ed35477467bcbb81a716
rhc07 <rorieclarke@gmail.com>
2024-12-02 21:56:28 +0000

    generate vacation idea change chain

--- chains/vacation.go
+++ chains/vacation.go
@@ -1,10 +1,16 @@
 package chains
 
 import (
+	"context"
 	"errors"
+	"log"
 	"slices"
+	"strings"
 
 	"github.com/google/uuid"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/prompts"
 )
 
 var Vacations []*Vacation
@@ -24,6 +30,67 @@ func GetVacationFromDb(id uuid.UUID) (Vacation, error) {
 	return *Vacations[idx], nil
 }
 
-func GenerateVacationIdeaChange(id uuid.UUID, budget int, idea string, hobbies []string) {
+func GenerateVacationIdeaChange(id uuid.UUID, budget int, season string, hobbies []string) {
+	log.Printf("Generating new vacation idea with ID: %s", id)
 
+	// Soft create the mock database instance
+	vacation := &Vacation{ID: id, Idea: "", Completed: false}
+	Vacations = append(Vacations, vacation)
+
+	// Connect to the openai LLM
+	ctx := context.Background()
+	llm, err := openai.New()
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return
+	}
+
+	// Create a system prompt with the season, hobbies, and budget parameters
+	// Helps tell the LLM how to act / respond to queries
+	systemPromptMessageString := "You are an AI travel agent that will help me create a vacation idea.\n" +
+		"My favorite season is {{.season}}.\n" +
+		"My hobbies include {{.hobbies}}.\n" +
+		"My budget is {{.budget}} dollars.\n"
+
+	systemPromptMessage := prompts.NewSystemMessagePromptTemplate(systemPromptMessageString, []string{"season", "hobbies", "dollars"})
+
+	// Create a human prompt with the request that a human would have
+	humanPromptMessageString := "Write up a travel itinerary"
+	humanPromptMessage := prompts.NewHumanMessagePromptTemplate(humanPromptMessageString, []string{})
+
+	// Create a chat prompt consisting of the system messages and human messages
+	// At this point, we will also inject the values into the prompts
+	// and turn them into message content objects which we can feed through
+	// to our LLM
+	chatPrompt := prompts.NewChatPromptTemplate([]prompts.MessageFormatter{systemPromptMessage, humanPromptMessage})
+
+	vals := map[string]any{
+		"season":  season,
+		"budget":  budget,
+		"hobbies": strings.Join(hobbies, ","),
+	}
+
+	msgs, err := chatPrompt.FormatMessages(vals)
+
+	if err != nil {
+		log.Printf("Error formatting messages; %v", err)
+		return
+	}
+
+	content := []llms.MessageContent{
+		llms.TextParts(msgs[0].GetType(), msgs[0].GetContent()),
+		llms.TextParts(msgs[1].GetType(), msgs[1].GetContent()),
+	}
+
+	// Invoke the LLM with the messages
+	completion, err := llm.GenerateContent(ctx, content)
+
+	if err != nil {
+		log.Printf("Error invoking llm: %v", err)
+		return
+	}
+	vacation.Idea = completion.Choices[0].Content
+	vacation.Completed = true
+
+	log.Printf("Generation for %s is done!", vacation.ID)
 }
